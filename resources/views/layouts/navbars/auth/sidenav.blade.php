30a18693b04595c8835df4c176aac9949976d552
Javiera <jmunozv@ing.ucsc.cl>
2024-04-26 22:41:53 -0400

    CAMBIO EN LA BARRITA de configuracion

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -5,7 +5,7 @@
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="{{ route('home') }}"
             target="_blank">
-            <img src="./img/ICONO_BARRA.png" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="./img/ICONO_SINFONDO.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Configuración SunFlower</span>
         </a>
     </div>
