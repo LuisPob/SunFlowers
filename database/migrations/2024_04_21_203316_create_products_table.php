acb70958b552119a79ed86dde75c893c651963a1
Maritxiu <mcastrop@ing.ucsc.cl>
2024-05-04 15:29:32 -0400

    ajuste seeder productos

--- database/migrations/2024_04_21_203316_create_products_table.php
+++ database/migrations/2024_04_21_203316_create_products_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
+            $table->string('name');
+            $table->string('slug');
             $table->string('details')->nullable();
             $table->double('price');
             $table->double('shipping_cost');
