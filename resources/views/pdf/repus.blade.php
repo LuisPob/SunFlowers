eba5ad1c2e665ff5ee48ce33a1f99b35642c9e06
TomasFellTapia <tfell@ing.ucsc.cl>
2024-06-02 05:43:30 -0400

    vcgd
    gdgd

--- resources/views/pdf/repus.blade.php
+++ resources/views/pdf/repus.blade.php
@@ -20,10 +20,15 @@
 
             </thead>
             <tbody>
-                @foreach ($users as $user)
-                <td>{{$user->username}}</td>
-                <td>{{$user->email}}</td>
-            </tbody>
+  
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+        @endforeach
+   
+</tbody>
         </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
