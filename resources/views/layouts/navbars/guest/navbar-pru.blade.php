9a8accffa383920159e013b438c3da005e46fb15
Javiera <jmunozv@ing.ucsc.cl>
2024-06-26 19:44:40 -0400

    DIRECCIONAMIENTO

--- resources/views/layouts/navbars/guest/navbar-pru.blade.php
+++ resources/views/layouts/navbars/guest/navbar-pru.blade.php
@@ -32,21 +32,15 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link me-2" href="/">
-                                    <!-- <i class="fas fa-user-circle opacity-6 text-dark me-1"></i> -->
-                                    Categorias
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="/">
+                                <a class="nav-link me-2" href="#footer">
                                     <!-- <i class="fas fa-user-circle opacity-6 text-dark me-1"></i> -->
                                     Contacto
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link me-2" href="/">
+                                <a class="nav-link me-2" href="#footer">
                                     <!-- <i class="fas fa-user-circle opacity-6 text-dark me-1"></i> -->
-                                    Nosotros
+                                    Sobre Sunflower
                                 </a>
                             </li>
                             <li class="nav-item">
