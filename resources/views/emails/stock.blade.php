2c94fb3c22537fe6c668d9aacc62f7518510552c
Maritxiu <mcastrop@ing.ucsc.cl>
2024-07-03 12:28:33 -0400

    bajo stock

---
+++ resources/views/emails/stock.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>No te quedes sin stock</title>
+</head>
+<body>
+    <p>Hola {{ $company->company_name }},</p>
+    <p>Uno de tus productos tiene bajo stock. Revisa la tienda.</p>
+</body>
+</html>
